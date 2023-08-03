b67f87e1322a347504fdf1c61c6482d7fd61d41b
CoToYo <cty.works@gmail.com>
2023-08-03 17:08:16 -0400

    update NewsService by creating a Class structure for all API service methods

--- services/NewsService.py
+++ services/NewsService.py
@@ -3,100 +3,95 @@ from flask import jsonify
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
-
-"""
-Create a LLM object
-"""
-OPENAI_API_KEY = dotenv_values(".env").get("OPENAI_API_KEY")
-llm = OpenAI(temperature=0.9, openai_api_key=OPENAI_API_KEY)
-
-"""
-Return news without considering keywords
-"""
+from models.NewsModel import CybernewsDB
 
 
-def getNews():
-    # fixed format for answer
-    answer_format = "news content (data source, date)"
+class NewsService:
+    def __init__(self) -> None:
+        self.OPENAI_API_KEY = dotenv_values(".env").get("OPENAI_API_KEY")
+        self.llm = OpenAI(temperature=0.9, openai_api_key=self.OPENAI_API_KEY)
+        self.db = CybernewsDB()
 
-    # add prompt
-    prompt = PromptTemplate(
-        input_variables=["format"],
-        template="""
-        Please give me a list of the most recent cybersecurity news.
-        Indicate data source and date.
-        Return the answer with the format of '{format}'.
-        """,
-    )
-    news = llm(prompt.format(format=answer_format))
-    print(news)
+    """
+    Return news without considering keywords
+    """
 
-    # convert news data into JSON format
-    news_JSON = toJSON(news)
+    def getNews(self):
+        # fixed format for answer
+        answer_format = "news content (data source, date)"
 
-    return news_JSON
+        # add prompt
+        prompt = PromptTemplate(
+            input_variables=["format"],
+            template="""
+            Please give me a list of the most recent cybersecurity news.
+            Indicate data source and date.
+            Return the answer with the format of '{format}'.
+            """,
+        )
+        news = self.llm(prompt.format(format=answer_format))
+        print(news)
 
+        # convert news data into JSON format
+        news_JSON = self.toJSON(news)
 
-"""
-return news based on certain keywords
-"""
+        return news_JSON
 
+    """
+    return news based on certain keywords
+    """
 
-def getNewsWithKeywords(user_keywords):
-    # fixed format for answer
-    answer_format = "news content (data source, date)"
+    def getNewsWithKeywords(self, user_keywords):
+        # fixed format for answer
+        answer_format = "news content (data source, date)"
 
-    # add prompt
-    prompt = PromptTemplate(
-        input_variables=["keywords", "format"],
-        template="""
-        Please give me a list of the most recent cybersecurity news with keywords of {keywords}.
-        Indicate data source and date.
-        Return the answer with the format of '{format}'.
-        """,
-    )
-    news = llm(prompt.format(keywords=user_keywords, format=answer_format))
-    print(news)
+        # add prompt
+        prompt = PromptTemplate(
+            input_variables=["keywords", "format"],
+            template="""
+            Please give me a list of the most recent cybersecurity news with keywords of {keywords}.
+            Indicate data source and date.
+            Return the answer with the format of '{format}'.
+            """,
+        )
+        news = self.llm(prompt.format(keywords=user_keywords, format=answer_format))
+        print(news)
 
-    # convert news data into JSON format
-    news_JSON = toJSON(news)
+        # convert news data into JSON format
+        news_JSON = self.toJSON(news)
 
-    return news_JSON
+        return news_JSON
 
+    """
+    deal requests with wrong route
+    """
 
-"""
-deal requests with wrong route
-"""
+    def notFound(self, error):
+        return jsonify({"error": "Not Found"}), 404
 
+    """
+    Convert news given by OpenAI API into JSON format.
+    """
 
-def notFound(error):
-    return jsonify({"error": "Not Found"}), 404
+    def toJSON(self, data: str):
+        news_list = data.split("\n")
+        news_list_json = []
 
+        for item in news_list:
+            # Avoid dirty data
+            if len(item) == 0:
+                continue
+            # Split the string at the first occurrence of '('
+            title, remaining = item.split("(", 1)
 
-"""
-Convert news given by OpenAI API into JSON format.
-"""
+            # Extract the source by splitting at ',' and removing leading/trailing whitespace
+            source = remaining.split(",")[0].strip()
 
+            # Extract the date by splitting at ',' and removing leading/trailing whitespace
+            date = remaining.split(",")[1].strip().rstrip(")")
 
-def toJSON(data: str):
-    news_list = data.split("\n")
-    news_list_json = []
+            # Create a dictionary for each news item and append it to the news_list
+            news_item = {"title": title.strip(), "source": source, "date": date}
+            news_list_json.append(news_item)
 
-    for item in news_list:
-        # Avoid dirty data
-        if len(item) == 0:
-            continue
-        # Split the string at the first occurrence of '('
-        title, remaining = item.split("(", 1)
-
-        # Extract the source by splitting at ',' and removing leading/trailing whitespace
-        source = remaining.split(",")[0].strip()
-
-        # Extract the date by splitting at ',' and removing leading/trailing whitespace
-        date = remaining.split(",")[1].strip().rstrip(")")
-
-        # Create a dictionary for each news item and append it to the news_list
-        news_item = {"title": title.strip(), "source": source, "date": date}
-        news_list_json.append(news_item)
-
-    return jsonify(news_list_json)
+        return jsonify(news_list_json)
