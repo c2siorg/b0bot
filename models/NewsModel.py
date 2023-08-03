e25ccf79ac3866b5596217ccfc83c87cc4788e7b
CoToYo <cty.works@gmail.com>
2023-08-03 17:07:08 -0400

    update NewsModel by creating a Class structure for initializing CyberDB object

--- models/NewsModel.py
+++ models/NewsModel.py
@@ -1,7 +1,14 @@
 from config.Database import client
 
-db = client.CybernewsDB
-
 """
 TODO: Read news from database.
 """
+
+
+class CybernewsDB:
+    def __init__(self):
+        self.client = client
+        self.db = self.client["CybernewsDB"]
+
+    def get_collections(self, collection_name):
+        return self.db.get_collection(collection_name)
