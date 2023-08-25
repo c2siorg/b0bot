b794e0bc32f3a3c8164548070ba27f4fe7abcab2
CoToYo <cty.works@gmail.com>
2023-08-25 15:01:26 -0400

    upadte db connection string

--- config/Database.py
+++ config/Database.py
@@ -3,4 +3,4 @@ from pymongo import MongoClient
 
 DB_PASSWORD = dotenv_values(".env").get("DB_PASSWORD")
 
-client = MongoClient(f"mongodb+srv://b0bot:{DB_PASSWORD}@cluster0.zqgexb4.mongodb.net/")
+client = MongoClient(f"mongodb+srv://b0bot:{DB_PASSWORD}@cluster0.zqgexb4.mongodb.net/?retryWrites=true&w=majority ")
