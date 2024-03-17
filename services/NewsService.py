a5a0b4e219b4676c491e83a79ad9f5120be56097
hardik1408 <hardikj22@iitk.ac.in>
2024-03-17 17:56:26 +0530

    added huggingface endpoint to use open LLMs

--- services/NewsService.py
+++ services/NewsService.py
@@ -1,21 +1,27 @@
+import os
 from dotenv import dotenv_values
 from flask import jsonify
-
-# import json
-from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
-from langchain.schema import HumanMessage, SystemMessage, AIMessage
-from langchain.chains import LLMChain, SimpleSequentialChain
+from langchain_community.llms import HuggingFaceEndpoint
+
 from models.NewsModel import CybernewsDB
-
-
+env_vars = dotenv_values(".env")
+HUGGINGFACEHUB_API_TOKEN = env_vars.get("HUGGINGFACE_TOKEN")
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
 class NewsService:
     def __init__(self) -> None:
         self.db = CybernewsDB()
-        self.OPENAI_API_KEY = dotenv_values(".env").get("OPENAI_API_KEY")
-        self.llm = ChatOpenAI(temperature=0.1, openai_api_key=self.OPENAI_API_KEY)
+        # self.OPENAI_API_KEY = dotenv_values(".env").get("OPENAI_API_KEY")
+        # self.llm = ChatOpenAI(temperature=0.1, openai_api_key=self.OPENAI_API_KEY)
         # fixed format for answer
+        # Load environment variables from .env file
+        
+        repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
+
+        self.llm = HuggingFaceEndpoint(
+                repo_id=repo_id, max_length=64, temperature=0.5, token=HUGGINGFACEHUB_API_TOKEN
+            )
         self.news_format = "title [source, date(MM/DD/YYYY), news url];"
         self.news_number = 10
 
@@ -39,33 +45,37 @@ class NewsService:
         )
         news_data = list(news_data)
 
-        news = self.llm(
-            [
-                SystemMessage(
-                    content="You are a news analyzer that will read the given cyber security news and return the answer based on the user's requirement."
-                ),
-                HumanMessage(content=str(news_data)),
-                AIMessage(
-                    content="I have read the news you provided. Now please tell me your request."
-                ),
-                HumanMessage(
-                    content="""Please give me a list of the most recent cybersecurity news based on the news given above.
-                    Indicate the source and date.
-                    Each news should be strictly in the format of {news_format}.
-                    Your reply should be news-only, without adding any of your conversational content.
-                    Do not use bullet points.
-                    Do not stop generating the answer until it is complete.
-                    Return at most {news_number} news.""".format(
-                        news_format=self.news_format, news_number=self.news_number
-                    )
-                ),
-            ]
-        )
+        template = """Question: {question}
+        Answer: Let's think step by step."""
 
-        print(news.content)
+        prompt = PromptTemplate.from_template(template)
+
+        messages = [
+            {
+                "role": "system",
+                "content": "You are a news analyzer that will read the given cyber security news and return the answer based on the user's requirement.",
+            },
+            {"role": "user", "content": str(news_data)},
+            {"role": "assistant","content":"I have read the news you provided. Now please tell me your request."},
+            {"role": "user","content":"""Please give me a list of the most recent cybersecurity news based on the news given above.
+                            Indicate the source and date.
+                            Each news should be strictly in the format of {news_format}.
+                            Your reply should be news-only, without adding any of your conversational content.
+                            Do not use bullet points.
+                            Do not use regex.
+                            Only give the output in the required format.
+                            Do not stop generating the answer until it is complete.
+                            Return at most {news_number} news.""".format(
+                                news_format=self.news_format, news_number=self.news_number
+                            )}
+        ]
+
+        llm_chain = LLMChain(prompt=prompt, llm=self.llm)
+        output=llm_chain.run(messages)
+        print(output)
 
         # convert news data into JSON format
-        news_JSON = self.toJSON(news.content)
+        news_JSON = self.toJSON(output)
 
         return news_JSON
 
